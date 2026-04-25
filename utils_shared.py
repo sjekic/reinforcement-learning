ede7e470fc6ca767aa4bc40a76778487ace3bcd4
Sofiia Avetisian <savetisian.ieu2023@student.ie.edu>
2026-04-25 23:44:16 +0200

    added continuos space model

--- utils_shared.py
+++ utils_shared.py
@@ -148,6 +148,8 @@ def collect_trajectories(env, get_action_fn, n_episodes=20, max_steps=999):
         steps = 0
         while not done and steps < max_steps:
             action = get_action_fn(state)
+            if np.isscalar(action):
+                action = np.array([action], dtype=np.float32)
             state, reward, terminated, truncated, _ = env.step(action)
             states.append(state.copy())
             total_reward += reward
